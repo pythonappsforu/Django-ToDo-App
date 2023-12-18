467f7766fa3a60ee866d723927a16caf67136cf8
pythonbyfebash <pythonbyfebash@gmail.com>
2023-12-18 17:26:43 +0530

    mark as done and undone feature added

--- todo_main/views.py
+++ todo_main/views.py
@@ -3,7 +3,7 @@ from todos.models import Task
 
 
 def home(request):
-    tasks = Task.objects.filter(is_completed=False)
+    tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
     completed_tasks = Task.objects.filter(is_completed=True)
     context = {
         'tasks': tasks,
